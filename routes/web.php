0d64d726e7e46220fef0102201d148a44607282d
David White <r.code13@yahoo.com>
2017-11-18 11:59:19 -0500

    Testing the Project

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'api'], function() {
+  // protected routes here
+ Route::resource('movies','Films\FilmsController');
+
+});
